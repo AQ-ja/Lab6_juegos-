eb5fdbb01e1701c5048eecb2c567dea87c20309b
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2020-05-10 12:09:19 -0600

    ...

---
+++ Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision col)
+    {
+        if(col.gameObject.tag.Equals("Player"))
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerMomevent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMomevent : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 1f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
